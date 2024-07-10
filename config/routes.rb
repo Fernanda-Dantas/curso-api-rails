379cf18ed0d7e455b1160f3e75479fbf2e0beb26
Fernanda <fer.dantas07@gmail.com>
2024-07-10 14:20:18 -0300

    refatorei o kinds_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :kinds
   # atalho dos recursos. É o que eu vou trabalhar. O nome do recurso deve estar no plural.
-  resources :contacts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  resources :contacts do
+    resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relationships/kind'
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
 end
