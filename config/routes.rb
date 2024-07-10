3f2c70b28a3109ac647f4cad1f3ea9c8306131d7
Fernanda <fer.dantas07@gmail.com>
2024-07-10 14:36:16 -0300

    fiz rotas do has_many em phones

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :kinds
   # atalho dos recursos. É o que eu vou trabalhar. O nome do recurso deve estar no plural.
-
-
   resources :contacts do
+
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
+
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/phones'
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
