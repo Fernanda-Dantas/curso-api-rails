05bbb06d4626c08590c86601eda3f9c8a95f1092
Fernanda <fer.dantas07@gmail.com>
2024-07-10 20:37:01 -0300

    fiz um crud no address com has_one

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,3 +1,8 @@
 class AddressSerializer < ActiveModel::Serializer
     attributes :id, :street, :city
+
+    #O endereço belongs a um contato
+    belongs_to :contact do
+      link(:related) { contact_address_url(object.contact.id) }
+    end
   end
