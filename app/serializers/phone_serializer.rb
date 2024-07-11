545d20008561a194cb23a6ee39e11cb38b989c47
Fernanda <fer.dantas07@gmail.com>
2024-07-10 21:03:11 -0300

    fiz um CRUD no phones com has_many

--- app/serializers/phone_serializer.rb
+++ app/serializers/phone_serializer.rb
@@ -1,4 +1,9 @@
 class PhoneSerializer < ActiveModel::Serializer
     attributes :id, :number
+
+        #O telefone belongs a um contato
+        belongs_to :contact do
+          link(:related) { contact_phones_url(object.contact.id) }
+        end
   end
   
